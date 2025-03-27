79341d57626530d1c1671b9021aca04c08251731
sail-stian <stian@sail.no>
2025-03-27 18:28:37 +0100

    chore: redundant lines

--- testing/dfmini-random.ino
+++ testing/dfmini-random.ino
@@ -1,101 +1,41 @@
 #include <SoftwareSerial.h>
-
 #include <DFRobotDFPlayerMini.h>
 
-
-
 // Set up the software serial for communication with DFPlayer Mini
-
 SoftwareSerial mySoftwareSerial(12, 13); // RX, TX
-
 DFRobotDFPlayerMini myDFPlayer;
 
-
-
 void setup() {
-
-  // Start the serial communication for debugging
-
-  Serial.begin(9600);
-
-
-
-  // Start the software serial communication
-
-  mySoftwareSerial.begin(9600);
-
-
+  Serial.begin(9600); // Start serial communication for debugging
+  mySoftwareSerial.begin(9600); // Start software serial communication
 
   // Initialize DFPlayer Mini
-
   if (!myDFPlayer.begin(mySoftwareSerial)) {
-
     Serial.println("DFPlayer Mini not detected!");
-
     while (true); // Stop if DFPlayer is not detected
-
   }
 
-
-
-  // Play a random song after initializing the player
-
-  playRandomSong();
-
+  playRandomSong(); // Play a random song after initializing the player
 }
-
-
 
 void loop() {
-
-  // You can call the playRandomSong function here if you want to play a random song in the loop.
-
-  // For example, play a random song every 10 seconds:
-
+  // Uncomment to play a random song every 10 seconds:
   // delay(10000); 
-
   // playRandomSong();
-
 }
 
-
-
 // Function to play a random song
-
 void playRandomSong() {
-
-  // Get the number of songs on the SD card
-
-  int totalSongs = myDFPlayer.readFileCounts() / 2;
-
-      Serial.print("Number of files: ");
-
-    Serial.println(totalSongs);
-
-  
+  int totalSongs = myDFPlayer.readFileCounts() / 2; // Get the number of songs on the SD card
+  Serial.print("Number of files: ");
+  Serial.println(totalSongs);
 
   if (totalSongs > 0) {
-
-    // Generate a random song number between 1 and the total number of songs
-
-    int randomSong = random(1, totalSongs + 1);
-
-    
-
+    int randomSong = random(1, totalSongs + 1); // Generate a random song number
     Serial.print("Playing song number: ");
-
     Serial.println(randomSong);
-
-
-
-    // Play the random song
-
-    myDFPlayer.play(randomSong);
-
+    myDFPlayer.play(randomSong); // Play the random song
   } else {
-
     Serial.println("No songs found on the SD card.");
-
   }
-
 }
